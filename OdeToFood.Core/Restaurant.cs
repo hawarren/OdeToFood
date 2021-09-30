257d6e7c105525782f971719b61a109f1bc25dd9
HWarren <HWarren@KR046045VM46>
2021-09-30 13:30:30 -0400

    Add project files.

---
+++ OdeToFood.Core/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Core
+{
+
+    public class Restaurant
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string Location { get; set; }
+        public CuisineType Cuisine { get; set; }
+    }
+}
