76d2e3a6358d35e61829f4b3289d52bbd0c5dccb
HWarren <HWarren@KR046045VM46>
2021-09-30 13:30:30 -0400

    Initial commit Part2 (Drilling into Data): Building a Data Access Service Add project files.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        public IEnumerable<Restaurant> GetAll() 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
