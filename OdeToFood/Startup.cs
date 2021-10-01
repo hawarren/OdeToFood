9d836e6f826b83365143ab4deff268972f1b263c
Hanif Warren <HWarren@Valleyhealth.com>
2021-10-01 11:27:53 -0400

    Completed Section 2: Drilling into data Next: Section 3 Working with models and model binding

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -8,7 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-//using OdeToFood.Data;
+using OdeToFood.Data;
+
 
 namespace OdeToFood
 {
@@ -24,7 +25,7 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-         //   services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
+            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
             services.AddRazorPages();
         }
 
