c2da4be243f0c9298e64118c8db59c623a960c3c
Hanif Warren <HWarren@Valleyhealth.com>
2021-10-01 10:46:48 -0400

    Fix project references to target .netcore 3.1 LTS

--- OdeToFood.Core/Restaurant.cs
+++ OdeToFood.Core/Restaurant.cs
@@ -7,9 +7,9 @@ namespace OdeToFood.Core
 
     public class Restaurant
     {
+        public int Id { get; set; }
         public string Name { get; set; }
-        public string Id { get; set; }
         public string Location { get; set; }
-        public CuisineType Cuisine { get; set; }
+        public CuisineType Cuisine { get; set; } 
     }
 }
--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using OdeToFood.Core;
+using System.Linq;
 
 namespace OdeToFood.Data
 {
@@ -12,9 +13,20 @@ namespace OdeToFood.Data
 
     public class InMemoryRestaurantData : IRestaurantData
     {
-        public IEnumerable<Restaurant> GetAll() 
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
         {
-            throw new NotImplementedException();
+            restaurants = new List<Restaurant>() { 
+            new Restaurant{Id = 1, Name = "Scott's Pizza", Location="Maryland" , Cuisine=CuisineType.Italian },
+            new Restaurant{Id = 1, Name = "Curry Shop", Location="Calcutta" , Cuisine=CuisineType.Indian },
+            new Restaurant{Id = 1, Name = "Jose's Burritos", Location="Cancun" , Cuisine=CuisineType.Mexican }
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
         }
     }
 }
