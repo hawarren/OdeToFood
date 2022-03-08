6e2f1c2fb73509109d90f7b8551b7d0d6104599d
Hanif Warren <hanif.warren@gmail.com>
2022-03-08 12:07:53 -0500

    Module 4: Building an edit form with tag helpers -Create edit form with dropdown list for valid cuisine types

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -2,21 +2,30 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OdeToFood.Data;
 using OdeToFood.Core;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class EditModel : PageModel
     {
         private readonly IRestaurantData restaurantData;
-        public Restaurant restaurant {get; set;}
-        public EditModel(IRestaurantData restaurantData){
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+        private readonly IHtmlHelper htmlHelper;
+
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
+        {
+            this.htmlHelper = htmlHelper;
             this.restaurantData = restaurantData;
 
         }
         public IActionResult OnGet(int restaurantId)
         {
-            restaurant = restaurantData.GetById(restaurantId);
-            if (restaurant == null)
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+
+            Restaurant = restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
             {
                 return RedirectToPage("./NotFound");
             }
