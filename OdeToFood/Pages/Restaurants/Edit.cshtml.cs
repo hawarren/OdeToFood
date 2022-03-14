0289966f47d77418f6003ffe09f4fabb3f70c28b
Hanif Warren <hanif.warren@gmail.com>
2022-03-14 12:21:54 -0400

    Module 3 Editing Data with Razor pages Module 3 Building a Create Restaurant Page Followed the POST-GET-REDIRECT Pattern for restaurant edit page. Fixed "all restaurants" button on details page. -Added example of reading message from configuration settings. -Added create page by modifying Edit page to handle restaurant creation

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -21,11 +21,17 @@ namespace OdeToFood.Pages.Restaurants
             this.restaurantData = restaurantData;
 
         }
-        public IActionResult OnGet(int restaurantId)
+        public IActionResult OnGet(int? restaurantId)
         {
             Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
-
-            Restaurant = restaurantData.GetById(restaurantId);
+            if (restaurantId.HasValue)
+            {
+                Restaurant = restaurantData.GetById(restaurantId.Value);
+            }
+            else{
+                Restaurant = new Restaurant();
+                
+            }
             if (Restaurant == null)
             {
                 return RedirectToPage("./NotFound");
@@ -37,6 +43,7 @@ namespace OdeToFood.Pages.Restaurants
             {
                 restaurantData.Update(Restaurant);
                 restaurantData.Commit();
+                return RedirectToPage("./Detail", new {restaurantId = Restaurant.Id});
 
             }
 
