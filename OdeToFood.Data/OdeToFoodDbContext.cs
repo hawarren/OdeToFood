e44bd24d1aff1bde1e856c8698e8d68788295bca
Hanif Warren <hanif.warren@gmail.com>
2022-03-19 20:07:23 -0400

    Module 5 working with SQL Server and the Entity Framework Core -Using Other Databases and Tools/Adding Connection Strings and Registering Services Installed and configured SQl server in docker. Added connection string and configured DBContext for accessing sql server instance.

--- OdeToFood.Data/OdeToFoodDbContext.cs
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -4,6 +4,11 @@ namespace OdeToFood.Data
 {
     public class OdeToFoodDbContext : DbContext
     {
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options)
+        : base(options)
+        {
+            
+        }
         public DbSet<Restaurant> Restaurants {get; set;}
     }
 }
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OdeToFood.Data;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace OdeToFood
@@ -25,6 +26,9 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<OdeToFoodDbContext>(options => {
+                options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb"));
+            });
             services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
             services.AddRazorPages();
         }
