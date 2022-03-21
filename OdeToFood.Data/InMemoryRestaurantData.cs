68432c77ad9cdcfad7e0702bdb55119ab21b928c
Hanif Warren <hanif.warren@gmail.com>
2022-03-21 19:16:22 -0400

    Module 5 working with SQL Server and the Entity Framework Core -Implementing a Data Access Service. Refactored InmemoryRestaurantData (placed into separate file). Delete method added

---
+++ OdeToFood.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using OdeToFood.Core;
+using System.Linq;
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>() { 
+            new Restaurant{Id = 1, Name = "Scott's Pizza", Location="Maryland" , Cuisine=CuisineType.Italian },
+            new Restaurant{Id = 2, Name = "Curry and Roti Shop", Location="Calcutta" , Cuisine=CuisineType.Indian },
+            new Restaurant{Id = 3, Name = "Jose's Burritos", Location="Cancun" , Cuisine=CuisineType.Mexican }
+            };
+        }
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.ToLower().StartsWith(name.ToLower())
+                   orderby r.Name
+                   select r;
+        }
+        public Restaurant GetById(int id) {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
+        }
+        public Restaurant Update(Restaurant updatedRestaurant){
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if (restaurant != null){
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+            return updatedRestaurant;
+
+        }
+        public int Commit(){
+            return 0;
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = restaurants.FirstOrDefault(r => r.Id == id);
+            if (restaurant != null)
+            {
+                restaurants.Remove(restaurant);
+            }
+                return restaurant;
+        }
+    }
+}
